c971aa9a51accb45ab2689a07c822013ab940a27
iffok247 <s.assougnon@gmail.com>
2023-04-17 17:12:53 +0200

    form update

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -35,6 +38,13 @@ class RegisterType extends AbstractType
                 'label'=>'Votre mot de passe',
                 'attr'=> [
                     'placeholder'=>'saisir votre mot de passe'
+                ]
+            ])
+            ->add('password_confirm',PasswordType::class,[
+                'label'=>'Confirmer votre mot de passe',
+                'mapped'=>false,
+                'attr'=> [
+                    'placeholder'=>'confirmer votre mot de passe'
                 ]
             ])
             ->add('submit',SubmitType::class,[
